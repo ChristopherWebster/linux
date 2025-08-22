e381a6ecd7addda2039051ca0f38826f242e58eb
Chris Webster <10155368+ChristopherWebster@users.noreply.github.com>
2025-08-22 12:32:46 +0100

    Add color formatting to error messages in test.sh
    Add color formatting to error messages in test.sh with defaults for colours and intense colours

--- test.sh
+++ test.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+#formatting:
+
+# Reset
+Colour_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White 
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+set -e
 
 #Init local vars
 RET=0
@@ -17,7 +43,7 @@ trap 'handle_error' ERR
 #############################################################
 usage()
 {
-	echo "usage: test -p /my/path/here   "
+	echo -e "${IRed}usage${Colour_Off}: ${ICyan}test -p ${IYellow}/my/path/here${Colour_Off}   "
 }
 #############################################################
 # Error handling function
@@ -44,7 +70,7 @@ loadArgs()
         ;;
 
       \?)
-      	echo "Error: incorrect parameter provided"
+      	echo -e "${IRed}Error: incorrect parameter provided${Colour_Off}"
         #usage
         usage >&2
         exit 1
